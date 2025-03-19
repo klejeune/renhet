8838d2301551ce8eba652a2336d28ca5b8c9ec19
Kevin Le Jeune <kevin-git@le-jeune.fr>
2025-03-19 18:42:55 +0100

    lint

--- src/Test.tsx
+++ src/Test.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react';
+import { useState } from 'react';
 import './App.css';
 import QuestionService from './questions/question.service';
 import QuestionState from './questions/questionState';
